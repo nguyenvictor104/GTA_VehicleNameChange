6738987014ab97286b322c7ad6884656bcfbe4cd
nguyenvictor104 <135561551+nguyenvictor104@users.noreply.github.com>
2023-11-19 21:53:54 -0500

    Refactored using PowerShell

---
+++ NameChange.ps1
@@ -0,0 +1,18 @@
+﻿# Prompt user for input
+$newName = Read-Host "Enter the new name for the files"
+
+# Get all files in the current directory excluding PowerShell files
+$files = Get-ChildItem -File | Where-Object { $_.Extension -ne '.ps1' }
+
+# Rename each file with the new name, appending "_hi" if the file name includes it
+foreach ($file in $files) {
+    $appendString = ""
+    if ($file.BaseName -like "*_hi*") {
+        $appendString = "_hi"
+    }
+
+    $newFileName = $newName + $appendString + $file.Extension
+    Rename-Item -Path $file.FullName -NewName $newFileName
+}
+
+Write-Host "Files renamed successfully."
